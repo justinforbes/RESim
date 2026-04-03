88de71b479c1fe2c72d87b9bf9b3b33c23ee7516
mike <mike>
2026-04-03 15:00:44 +0000

    get dhcp and dns logs if they exist

--- simics/bin/getDriverLogs.sh
+++ simics/bin/getDriverLogs.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
 mkdir -p logs/driver
+#ssh -p 4022 mike@localhost "journalctl -u start_driver_service.service >/tmp/driver_service.log"
+ssh -p 4022 mike@localhost "journalctl -u start_dhcp.service >/tmp/dhcp.log"
+ssh -p 4022 mike@localhost "journalctl -u start_dns.service >/tmp/dns.log"
 scp -P 4022 mike@localhost:/tmp/*.log logs/driver/
